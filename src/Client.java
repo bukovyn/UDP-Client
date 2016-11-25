534eabb3be50a6d902935179b609fa7023f67845
Nazar Bukovynsky <bukovyn@gmail.com>
2016-11-25 17:09:52 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -4,12 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.*;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
 import java.net.*;
 
 public class Client extends JFrame implements ActionListener{
